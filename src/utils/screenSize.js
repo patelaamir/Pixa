8ca9ded91a0ebb9f3baa400e77ea412249dd37ac
Aamir Patel <patelaamir1204@gmail.com>
2025-03-09 12:08:23 +0530

    fix: remove the window resize listener

--- src/utils/screenSize.js
+++ src/utils/screenSize.js
@@ -1,27 +1,11 @@
 // useScreenSize.js
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 const useScreenSize = () => {
   const [screenSize, setScreenSize] = useState({
     width: window.innerWidth,
     height: window.innerHeight,
   });
-
-  useEffect(() => {
-    const handleResize = () => {
-      setScreenSize({
-        width: window.innerWidth,
-        height: window.innerHeight,
-      });
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    // Clean up the event listener when the component unmounts
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
 
   return screenSize;
 };
